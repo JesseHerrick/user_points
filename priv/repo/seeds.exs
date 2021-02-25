d00e46b23160d52a706a961bac958ffeb130bbb2
Jesse Herrick <me@jesseherrick.com>
2021-02-25 12:05:47 -0500

    Add seeds to generate 100 users with 0 points

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,4 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     UserPoints.Repo.insert!(%UserPoints.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# Generates 100 seed users, each with 0 points.
+for _ <- 1..100 do
+  UserPoints.Accounts.create_user(%{points: 0})
+end
