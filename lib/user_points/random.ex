d463319e67b2da5e8da317c6f05d7ae590a7738a
Jesse Herrick <me@jesseherrick.com>
2021-02-25 12:08:13 -0500

    Adds random number generator module for generating numbers from 0 to 100

---
+++ lib/user_points/random.ex
@@ -0,0 +1,14 @@
+defmodule UserPoints.Random do
+  @moduledoc """
+  Module for generating uniform random numbers.
+  """
+
+  @max_number 100
+
+  @doc """
+  Generates a random number between 0 and `@max_number`, which is currently 100.
+  """
+  def random_points do
+    :random.uniform(@max_number + 1) - 1
+  end
+end
